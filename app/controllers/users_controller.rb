26e83618c04d3297f789ae80b798d9886c2e72a6
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-17 15:52:38 +0000

    Try and Error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @microposts=@user.microposts.all
+    @microposts=@user.microposts
     # @micropost=@user.microposts.first
   end
 
